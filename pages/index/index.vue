2ecf94cac4435ff6b8cae8bf9187eb88c03379ed
tangwenjing <tangwenjing@7k7k.com>
2018-11-09 09:31:42 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="content">
+        <text class="title">{{title}}</text>
+        <text v-on:click="changeMessage">{{message}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+                message: "tom"
+			};
+		},
+		methods() {
+
+        }
+	}
+</script>
+
+<style>
+    @import "../common/common.css";
+	.content {
+		flex: 1;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
